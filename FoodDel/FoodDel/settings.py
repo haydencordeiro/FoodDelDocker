718efd42862a4c2e2d96acfa3b666f531afad6e9
Hayden Corderio <cordeirohayden@gmail.com>
2021-04-01 14:19:41 +0530

    updated static path

--- FoodDel/FoodDel/settings.py
+++ FoodDel/FoodDel/settings.py
@@ -124,8 +124,15 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 
