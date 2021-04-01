076b73bb26f05d7b0679aadb8c7a5c2c7988c225
Hayden Corderio <cordeirohayden@gmail.com>
2021-04-01 15:36:00 +0530

    ngix serving static files

--- FoodDel/FoodDel/settings.py
+++ FoodDel/FoodDel/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'FoodDel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,19 +124,13 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# MEDIA_URL = '/media/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # rest framework
 REST_FRAMEWORK = {
